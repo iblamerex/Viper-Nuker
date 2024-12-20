4f3a1034798c9f00d2850b659f0e3ae2ffc15db7
ƦƐ✘ <161481850+iblamerex@users.noreply.github.com>
2024-12-20 18:09:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+SERVER = {
+    "new_name": "ReXx Wizzed You",  
+    "new_description": "ReXx Wizzed You",  
+}
+
+WEBHOOK = {"name": "ReXx Wizzed You",}
+
+FULL_NUKE = {
+    'channel_number': 20,  
+    'channel_type': 'text',   
+    'channel_name': 'Nuked By ReXx',  
+    'num_messages': 60,   
+    'message_content': '@everyone nuked by https://discord.gg/makXEQk2TF' 
+}
+
+EMBED = {
+    "title": "ReXx Wizzed You",    
+    "description": "ReXx Wizzed You",            
+    "color": 0xFF5733,      
+    "fields": [
+        {"name": "ReXx Wizzed You", "value": "ReXx Wizzed You", "inline": False},     
+        {"name": "ReXx Wizzed You", "value": "ReXx Wizzed You", "inline": False},
+        {"name": "ReXx Wizzed You", "value": "ReXx Wizzed You", "inline": False},    
+    ],
+        "footer": "ReXx Wizzed You",     
+}
